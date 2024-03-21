3d9926c24fd982feb4f9857de535b71c025e2572
Nguyen Tran <nctran108@gmail.com>
2024-03-21 00:58:20 -0400

    make some change

--- networks/large.py
+++ networks/large.py
@@ -4,31 +4,31 @@ from keras.layers import Conv2D, ZeroPadding2D
 def layers(input_shape):
     return [
         ZeroPadding2D((3,3), input_shape=input_shape, data_format='channels_first'),
-        Conv2D(filters=64,kernel_size=(7,7), padding='valid', data_format='channels_first'),
+        Conv2D(64,kernel_size=(7,7), padding='valid', data_format='channels_first'),
         Activation('relu'),
 
         ZeroPadding2D((2,2), data_format='channels_first'),
-        Conv2D(filters=64,kernel_size=(5,5), data_format='channels_first'),
+        Conv2D(64,kernel_size=(5,5), data_format='channels_first'),
         Activation('relu'),
 
         ZeroPadding2D((2,2), data_format='channels_first'),
-        Conv2D(filters=64,kernel_size=(5,5), data_format='channels_first'),
+        Conv2D(64,kernel_size=(5,5), data_format='channels_first'),
         Activation('relu'),
 
         ZeroPadding2D((2,2), data_format='channels_first'),
-        Conv2D(filters=48,kernel_size=(5,5), data_format='channels_first'),
+        Conv2D(48,kernel_size=(5,5), data_format='channels_first'),
         Activation('relu'),
 
         ZeroPadding2D((2,2), data_format='channels_first'),
-        Conv2D(filters=48,kernel_size=(5,5), data_format='channels_first'),
+        Conv2D(48,kernel_size=(5,5), data_format='channels_first'),
         Activation('relu'),
 
         ZeroPadding2D((2,2), data_format='channels_first'),
-        Conv2D(filters=32,kernel_size=(5,5), data_format='channels_first'),
+        Conv2D(32,kernel_size=(5,5), data_format='channels_first'),
         Activation('relu'),
 
         ZeroPadding2D((2,2), data_format='channels_first'),
-        Conv2D(filters=32,kernel_size=(5,5), data_format='channels_first'),
+        Conv2D(32,kernel_size=(5,5), data_format='channels_first'),
         Activation('relu'),
 
         Flatten(),
--- train_bot.py
+++ train_bot.py
@@ -14,14 +14,14 @@ if __name__ == '__main__':
     encoder = SevenPlaneEncoder((go_board_size,go_board_size))
     processor = GoDataProcessor(encoder=encoder.name())
 
-    generator = processor.load_go_data(num_samples=100,use_generator=True)
+    generator = processor.load_go_data(num_samples=15000,use_generator=True)
     generator_test = processor.load_go_data('test',num_samples=100,use_generator=True)
     print("Got features and layers")
 
     input_shape = (encoder.num_planes, go_board_size, go_board_size)
     
-    model = Sequential()
     network_layers = large.layers(input_shape=input_shape)
+    model = Sequential()
 
     for layer in network_layers:
         model.add(layer)
@@ -29,8 +29,8 @@ if __name__ == '__main__':
     model.summary()
 
     model.compile(loss='categorical_crossentropy',
-                optimizer='adadelta',
-                metrics=['accuracy'])
+                  optimizer='adadelta',
+                  metrics=['accuracy'])
     
     epochs = 20
     batch_size = 128
@@ -39,18 +39,9 @@ if __name__ == '__main__':
               epochs=epochs,
               steps_per_epoch=generator.get_num_samples(batch_size,num_classes) / batch_size,
               validation_data=generator_test.generate(batch_size,num_classes),
-              validation_steps=generator_test.get_num_samples() / batch_size,
+              validation_steps=generator_test.get_num_samples(batch_size,num_classes) / batch_size,
               callbacks=[ModelCheckpoint('../checkpoints/large_model_epoch_{epoch}.keras')]
               )
-    """
-    model.fit_generator(generator=generator.generate(batch_size,num_classes),
-                epochs=epochs,
-                steps_per_epoch=generator.get_num_samples() / batch_size,
-                validation_data=generator_test.generate(batch_size,num_classes),
-                validation_steps=generator_test.get_num_samples() / batch_size,
-                callbacks=ModelCheckpoint('../checkpoints/large_model_epoch_{epoch}.h5'))
-    """
-    print("Finished fitting....")
             
     model.evaluate(generator_test.generate(batch_size,num_classes),
                    steps=generator_test.get_num_samples(batch_size,num_classes) / batch_size)
@@ -59,6 +50,6 @@ if __name__ == '__main__':
     h5file = h5py.File("./go/agent/deep_bot.h5", 'w')
     deep_learning_bot.serialize( h5file)
 
-    model_file = h5py.File("./go/agent/deep_bot.h5", 'r')
-    bot_from_file = load_prediction_agent(model_file)
+    #model_file = h5py.File("./go/agent/deep_bot.h5", 'r')
+    #bot_from_file = load_prediction_agent(model_file)
     
