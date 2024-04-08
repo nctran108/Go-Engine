2a07d3c11f53e0007d8247b4e7625b20c58199a0
Nguyen Tran <nctran108@gmail.com>
2024-04-08 13:10:25 -0400

    modified the file

--- geanerate_train_data.py
+++ geanerate_train_data.py
@@ -2,18 +2,20 @@ from go.data.parallel_processor import GoDataProcessor
 from go.encoders.alphago import AlphaGoEncoder
 
 rows, cols = 19, 19
-num_classes = rows * cols
-num_games = 10000
-
 encoder = AlphaGoEncoder((rows,cols))
 processor = GoDataProcessor(encoder=encoder.name())
 
-# generate train
-samples = processor.generate_samples('train', num_games)
-processor.map_to_workers('train', samples)
-processor.load_data_from_npy('train')
+def generate_sample(data_type='train', num_games=1000):
+    samples = processor.generate_samples(data_type, num_games)
+    processor.map_to_workers(data_type, samples)
+    processor.load_data_from_npy(data_type)
 
-# generate test
-samples = processor.generate_samples('test', num_games)
-processor.map_to_workers('test', samples)
-processor.load_data_from_npy('test')
+def main():
+    num_games = 10000
+    # generate train
+    generate_sample('train', num_games)
+    # generate test
+    generate_sample('test', num_games)
+
+if __name__ == '__main__':
+    main()
