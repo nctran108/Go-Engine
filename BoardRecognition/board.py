73e722f747542a17b7cb66e7f1b6eef0c767bcb7
Nguyen Tran <60759653+nctran108@users.noreply.github.com>
2026-06-07 10:46:49 -0400

    change

--- BoardRecognition/board.py
+++ BoardRecognition/board.py
@@ -1,24 +1,16 @@
 import cv2
 import numpy as np
 import image_processing
+import matplotlib.pyplot as plt
+import utils
 
-class BoardRecognition:
-    def __init__(self, board_size):
-        self.board_size = board_size
+def main():
+    input = cv2.imread("./data/Zrzut ekranu 2020-04-11 o 15.42.14.png", cv2.IMREAD_COLOR)
 
-    def process_image(self, image):
-        # get 4 conners of the board, either manually or using opencv
-        # assume the board is static and not move because this will give many more problem,
-        # this can be something in the future for more robust version.
-        coners = image_processing.conner_detector(image)
-        # assume the board in board size that input by the user rather than detect from the real board for easier programming
-        # can add detect lines to guess the board size in future when find a good robust method.
+    preProcess_img = image_processing.PreProcessImage().process(input)
 
-        # transform the board into up-side down perspective to have better view on the board
+    board = image_processing.BoardRecognition()
+    board.findFourCorners(preProcess_img)
 
-        # draw lines base on board size and gett all coordinate of crossing points
-
-        # detect the stones colors and positions base on crossing points
-
-        # return the computer version of board game
-        return
+if __name__ == "__main__":
+    main()
